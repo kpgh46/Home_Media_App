68517b41661134e61f2d546b5048876265af15b3
Kevin McPeak <kevmc46@gmail.com>
2023-11-09 19:39:39 -0500

    updated css with BEM

---
+++ backend/models/Photo.js
@@ -0,0 +1,13 @@
+class Photo {
+	constructor(id, type, month, year, url, favorite, tags) {
+		this.id;
+		this.type;
+		this.month;
+		this.year;
+		this.url;
+		this.favorite;
+		this.tags;
+	}
+}
+
+module.exports = Photo;
