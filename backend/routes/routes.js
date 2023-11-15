228bbc162c87d7c00f84144dff93b103c49282e3
Kevin McPeak <kevmc46@gmail.com>
2023-11-14 22:55:04 -0500

    added upload post method for file upload

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const homeController = require("../controllers/homeController");
+const { getmain, upload } = require("../controllers/homeController");
 
-router.get("/", homeController.getmain);
+router.get("/", getmain);
+
+router.post("/upload", upload.single("picture"), (req, res) => {
+	res.send("correct");
+});
 
 module.exports = router;
